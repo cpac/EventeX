c57c8b4c0fd0662ca84d271fb98d77491566c955
unknown <cursodjangocpac@gmail.com>
2011-05-15 12:09:30 -0300

    12th commit

--- urls.py
+++ urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
     
-    (r'^$',homepage),
+    #(r'^$',homepage),
 )
 
 # urlpatterns += staticfiles_urlpatterns()
