a3647af38132e8853fe5623c941b79c7491c23bf
unknown <cursodjangocpac@gmail.com>
2011-05-28 23:42:57 -0300

    Fase 03.06

--- subscription/forms.py
+++ subscription/forms.py
@@ -5,8 +5,14 @@ from django import forms
 
 from subscription.models import Subscription
 
-class SubscriptionForm(forms.ModelForm) :
+#class SubscriptionForm(forms.ModelForm) :
 
-    class Meta:
-        model = Subscription
-        exclude = ('created_at',)
+#    class Meta:
+#        model = Subscription
+#        exclude = ('created_at',)
+
+class SubscriptionForm(forms.Form):
+        name = forms.CharField()
+        cpf = forms.CharField()
+        email = forms.EmailField()
+        phone = forms.CharField()
