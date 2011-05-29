4391a1e7e0a48a84afaf238fc200efac07fa31a9
unknown <cursodjangocpac@gmail.com>
2011-05-29 00:51:26 -0300

    Fase 03.12

--- subscription/forms.py
+++ subscription/forms.py
@@ -2,12 +2,57 @@
 # Create your views here.
 
 from django import forms
+from django.forms.fields import EMPTY_VALUES
 
 from subscription.models import Subscription
 from subscription.validators import CpfValidator
 
 from django.utils.translation import ugettext as _
-from subscription import validators
+# from subscription import validators
+
+class PhoneWidget(forms.MultiWidget):
+
+    def __init__(self, attrs=None):
+
+        widgets = (
+            forms.TextInput(attrs=attrs),
+            forms.TextInput(attrs=attrs)
+        )
+
+        super(PhoneWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+
+        if not value:
+            return [None, None]
+        
+        return value.split('-')
+
+class PhoneField(forms.MultiValueField):
+    widget = PhoneWidget
+
+    def __init__(self, *args, **kwargs):
+
+        fields = (
+            forms.IntegerField(),
+            forms.IntegerField()
+        )
+
+        super(PhoneField, self).__init__(fields, *args, **kwargs)
+
+    def compress(self, data_list):
+
+        if not data_list:
+            return none
+
+        if data_list[0] in EMPTY_VALUES:
+            raise forms.ValidationError(u'DDD inválido.')
+
+        if data_list[1] in EMPTY_VALUES:
+            raise forms.ValidationError(u'Número inválido.')
+
+        return '%s-%s' % tuple(data_list)
+
 
 #class SubscriptionForm(forms.ModelForm) :
 
@@ -15,11 +60,13 @@ from subscription import validators
 #        model = Subscription
 #        exclude = ('created_at',)
 
+
 class SubscriptionForm(forms.Form):
         name = forms.CharField(label=_('Nome'), max_length=100)
         cpf = forms.CharField(label=_('CPF'), validators=[CpfValidator])
         email = forms.EmailField(label=_('E-mail'),required=False)
-        phone = forms.CharField(label=_('Telefone'), required=False,max_length=20)
+        phone = PhoneField(label=_('Telefone'), required=False,max_length=20)
+                #forms.CharField(label=_('Telefone'), required=False,max_length=20)
 
         def _unique_check(self, fieldname, error_message):
 
