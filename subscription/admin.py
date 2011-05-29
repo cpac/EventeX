b101cdb57d6d3e26f746e864b634f7aabfaa6dd5
unknown <cursodjangocpac@gmail.com>
2011-05-28 23:14:27 -0300

    Fase 03.03

--- subscription/admin.py
+++ subscription/admin.py
@@ -6,6 +6,9 @@ from django.contrib import admin
 
 from subscription.models import Subscription
 
+from django.utils.translation import ugettext as _
+from django.utils.translation import ungettext
+
 class SubscriptionAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'cpf', 'email', 'phone', 'created_at','subscribed_today')
@@ -18,5 +21,20 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     subscribed_today.short_description = 'Inscrito"hoje?'
     subscribed_today.boolean = True
+
+    actions =['mark_as_paid']
+
+    def mark_as_paid(self, request,queryset):
+
+        count = queryset.update(paid=True)
+
+        msg = ungettext(
+                        u'%(count)d inscrição foi marcada como paga.',
+                        u'%(count)d inscrições foram marcadas como pagas.',
+                        count ) % {'count':count}
+
+        self.message_user(request,msg)
+
+    mark_as_paid.short_description =_(u'Marcar como pagas')
 
 admin.site.register(Subscription, SubscriptionAdmin)
