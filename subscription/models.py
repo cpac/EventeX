2282ded5722b4ffff2896398a2249a99bd0d86fc
unknown <cursodjangocpac@gmail.com>
2011-05-29 01:39:45 -0300

    Fase 03.18

--- subscription/models.py
+++ subscription/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Subscription(models.Model):
     name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
-    email = models.EmailField(unique=True,blank=True)
+    email = models.EmailField(blank=True)
     phone = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField()
