a8bfbe2aea880b7d7d775d64b6f9d4a76d9d4d63
unknown <cursodjangocpac@gmail.com>
2011-05-29 00:02:15 -0300

    Fase 03.08

--- subscription/forms.py
+++ subscription/forms.py
@@ -4,8 +4,10 @@
 from django import forms
 
 #from subscription.models import Subscription
+from subscription.validators import CpfValidator
 
 from django.utils.translation import ugettext as _
+from subscription import validators
 
 #class SubscriptionForm(forms.ModelForm) :
 
@@ -15,6 +17,6 @@ from django.utils.translation import ugettext as _
 
 class SubscriptionForm(forms.Form):
         name = forms.CharField(label=_('Nome'), max_length=100)
-        cpf = forms.CharField(label=_('CPF'), max_length=11,min_length=11)
+        cpf = forms.CharField(label=_('CPF'), validators=[CpfValidator])
         email = forms.EmailField(label=_('E-mail'))
         phone = forms.CharField(label=_('Telefone'), required=False,max_length=20)
