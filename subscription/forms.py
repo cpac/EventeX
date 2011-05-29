4ed0173cef088bb09e79fcc4ae7cad28d3a1cdd9
unknown <cursodjangocpac@gmail.com>
2011-05-29 00:37:18 -0300

    Fase 03.11

--- subscription/forms.py
+++ subscription/forms.py
@@ -18,7 +18,7 @@ from subscription import validators
 class SubscriptionForm(forms.Form):
         name = forms.CharField(label=_('Nome'), max_length=100)
         cpf = forms.CharField(label=_('CPF'), validators=[CpfValidator])
-        email = forms.EmailField(label=_('E-mail'))
+        email = forms.EmailField(label=_('E-mail'),required=False)
         phone = forms.CharField(label=_('Telefone'), required=False,max_length=20)
 
         def _unique_check(self, fieldname, error_message):
